001cd00a9b05054f69f48dfc57f5cb2507d28c46
allie <aferas@anthology.com>
2024-04-11 14:58:25 -0400

    utility for calculating workload

---
+++ notebooks/utils.py
@@ -0,0 +1,16 @@
+from tqdm import tqdm
+import pandas as pd
+
+def time_series_workload(df, start_col='Open Date', end_col='Closed Date', label_col='Subject'):
+
+    alldates = pd.date_range(start=df[start_col].min(),end=df[end_col].max(),freq='D')
+    dates, labels = [], []
+    for d in tqdm(alldates):
+        tmp = df[(df[start_col] <= d)&(df[end_col] >= d)][label_col].tolist()
+        dates += [d]*len(tmp)
+        labels += tmp
+
+    workload = pd.DataFrame(zip(*[dates,labels]), columns=['date', 'label'])
+    workload = workload.groupby('date').value_counts().reset_index()
+
+    return workload
