40c3c0c65f7b629f4043a0063218c1c32ddf80f7
allie <aferas@anthology.com>
2024-04-18 10:34:06 -0400

    fix workload function to include 0s

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -6,12 +6,18 @@ def time_series_workload(df, start_col='Open Date', end_col='Closed Date', label
 
     alldates = pd.date_range(start=df[start_col].min(),end=df[end_col].max(),freq='D')
     dates, labels = [], []
-    for d in tqdm(alldates):
+    for d in tqdm(alldates, desc="Collecting active records by date"):
         tmp = df[(df[start_col] <= d)&(df[end_col] >= d)][label_col].tolist()
         dates += [d]*len(tmp)
         labels += tmp
 
+    print("Calculating the workload...")
     workload = pd.DataFrame(zip(*[dates,labels]), columns=['date', 'label'])
-    workload = workload.groupby('date').value_counts().reset_index()
+    workload = (
+        workload
+        .pivot_table(index='date', columns='label', aggfunc=len, fill_value=0)
+        .reset_index()
+        .melt(id_vars='date')
+    )
 
     return workload
