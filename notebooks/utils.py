fbcb44768f001c01507e01b8fd4796b44ed7a757
allie <aferas@anthology.com>
2024-04-27 23:02:23 -0400

    workload including unclosed tickets

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -5,21 +5,31 @@ def time_series_workload(df, city_name, start_col='open_date', end_col='close_da
     """Function for adding the workload for time series. Dataframe should only contain one city."""
 
     alldates = pd.date_range(start=df[start_col].min(),end=df[end_col].max(),freq='D')
-    dates, labels = [], []
-    for d in tqdm(alldates, desc="Collecting active records by date"):
-        tmp = df[(df[start_col] <= d)&(df[end_col] >= d)][label_col].tolist()
-        dates += [d]*len(tmp)
-        labels += tmp
 
-    print("Calculating the workload...")
-    workload = pd.DataFrame(zip(*[dates,labels]), columns=['date', 'label'])
-    workload = (
-        workload
-        .pivot_table(index='date', columns='label', aggfunc=len, fill_value=0)
-        .reset_index()
-        .melt(id_vars='date')
+    final = (
+        pd.MultiIndex
+        .from_product([alldates,df['department'].unique()], names=['date','department'])
+        .to_frame(index=False)
     )
+    final['date'] = pd.to_datetime(final['date'].dt.date)
 
-    workload['city'] = city_name
+    counts = pd.DataFrame()
+    for d in tqdm(final['date'].unique()):
+        tmp = (
+            df[
+                (df['open_date'] <= d)
+                &(
+                    (df['close_date'] >= d)
+                    |(df['close_date'].isna())
+                )]['department']
+            .value_counts()
+            .reset_index()
+        )
+        tmp['date'] = d
+        counts = pd.concat([counts, tmp])
+    
+    final = final.merge(counts, how = 'left').fillna(0)
 
-    return workload
+    final['city'] = city_name
+
+    return final
