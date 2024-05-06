336dba3e67562b3ccc483bbd2541221445915521
allie <aferas@anthology.com>
2024-05-06 16:35:29 -0400

    updatin workload to handle tickets that open/close same day

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -4,14 +4,17 @@ import pandas as pd
 def time_series_workload(df, city_name, start_col='open_date', end_col='close_date', label_col='department'):
     """Function for adding the workload for time series. Dataframe should only contain one city."""
 
-    alldates = pd.date_range(start=df[start_col].min(),end=df[end_col].max(),freq='D')
+    alldates = pd.date_range(start=df['open_date'].dt.date.min(),end=df['close_date'].max(),freq='D')
 
     final = (
         pd.MultiIndex
         .from_product([alldates,df['department'].unique()], names=['date','department'])
         .to_frame(index=False)
     )
-    final['date'] = pd.to_datetime(final['date'].dt.date)
+    final['date'] = final['date'].dt.date
+
+    df['open_date'] = df['open_date'].dt.date
+    df['close_date'] = df['close_date'].dt.date
 
     counts = pd.DataFrame()
     for d in tqdm(final['date'].unique()):
