4fa9f938d72920b103e41ccfe76c38324a17a004
allie <aferas@anthology.com>
2024-05-12 12:46:20 -0400

    updates to model training, utils, and annotating new tickets

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -2,42 +2,9 @@ from tqdm import tqdm
 import pandas as pd
 import numpy as np
 from datetime import timedelta
-
-def time_series_workload(df, city_name, start_col='open_date', end_col='close_date', label_col='department'):
-    """Function for adding the workload for time series. Dataframe should only contain one city."""
-
-    alldates = pd.date_range(start=df[start_col].dt.date.min(),end=df[end_col].max(),freq='D')
-
-    final = (
-        pd.MultiIndex
-        .from_product([alldates,df[label_col].unique()], names=['date',label_col])
-        .to_frame(index=False)
-    )
-    final['date'] = final['date'].dt.date
-
-    df[start_col] = df[start_col].dt.date
-    df[end_col] = df[end_col].dt.date
-
-    counts = pd.DataFrame()
-    for d in tqdm(final['date'].unique()):
-        tmp = (
-            df[
-                (df[start_col] <= d)
-                &(
-                    (df[end_col] >= d)
-                    |(df[end_col].isna())
-                )][label_col]
-            .value_counts()
-            .reset_index()
-        )
-        tmp['date'] = d
-        counts = pd.concat([counts, tmp])
-    
-    final = final.merge(counts, how = 'left').fillna(0)
-
-    final['city'] = city_name
-
-    return final
+import statsmodels.api as sm
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from sklearn.base import BaseEstimator, RegressorMixin
 
 def time_series_newtix(df, city_name, start_col='open_date', end_col='close_date', label_col='department'):
     """Function for adding the workload for time series. Dataframe should only contain one city."""
